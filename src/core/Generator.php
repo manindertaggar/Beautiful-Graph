04c5042817692dce87d2e263c5995d3955c31c95
Maninder Taggar <manindersingh030@gmail.com>
2017-07-22 20:04:05 +0530

    added core code

---
+++ src/core/Generator.php
@@ -0,0 +1,21 @@
+<?php
+namespace Graph\core;
+
+public class Generator{
+	private $sentence;
+	private $wordmap;
+
+	public function __construct($sentence){
+		$this->sentence = $sentence;
+		$this->wordmap = json_decode(__DIR__."/wordmap.json");
+	}
+
+
+	public function getNext(){
+		$shouldPrint = false;
+		//TODO: add your code here
+		return $shouldPrint;
+	}
+
+
+}
